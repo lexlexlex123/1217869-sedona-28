feb78e52085b6183a3f9d8875fa3c1a5e213e9db
lexlexlex123 <44517871+lexlexlex123@users.noreply.github.com>
2023-05-20 06:42:46 +0300

    картинка на карту если не работает js

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,8 @@
 window.onload = function () {
   let menu = document.getElementsByClassName("main-nav__wrapper")[0];
+  let map = document.getElementsByClassName("map__image-static")[0];
+  map.style.display = "none";
+
   if (window.screen.width < 768) {
     menu.style.display = "none";
   }
